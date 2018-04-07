fc8edc50d754719230bd8008f4f450e9feba8331
Vaidotas Valuckas <vaidotas.valuckas@gmail.com>
2018-04-07 20:24:33 +0300

    Verify both serialization and deserialization of json resource

--- src/test/java/lt/rieske/payments/domain/PaymentSerializationTest.java
+++ src/test/java/lt/rieske/payments/domain/PaymentSerializationTest.java
@@ -3,7 +3,11 @@ package lt.rieske.payments.domain;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lt.rieske.payments.infrastructure.PaymentsObjectMapper;
+import org.apache.commons.codec.Charsets;
+import org.apache.commons.io.IOUtils;
+import org.json.JSONException;
 import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
 
 import java.io.IOException;
 import java.util.List;
@@ -13,12 +17,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class PaymentSerializationTest {
 
     @Test
-    public void deserializesExamplePayments() throws IOException {
+    public void deserializesAndSerializesExamplePayments() throws IOException, JSONException {
         ObjectMapper mapper = new PaymentsObjectMapper();
+        String paymentsJson = IOUtils.resourceToString("/fixtures/transactions.json", Charsets.UTF_8);
 
-        List<Payment> payments = mapper.readValue(getClass().getResourceAsStream("/fixtures/transactions.json"),
-                new TypeReference<List<Payment>>() {});
+        List<Payment> payments = mapper.readValue(paymentsJson, new TypeReference<List<Payment>>() {});
 
         assertThat(payments).hasSize(14);
+        JSONAssert.assertEquals(paymentsJson, mapper.writeValueAsString(payments), true);
     }
 }
